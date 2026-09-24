ff475bcef38717041bb7cd01dcd11427b378db62
jacke <jacke500@proton.me>
2026-09-24 09:34:53 +0200

    Add Lab8 exercise D

---
+++ Lab8/lesson8_exercises_part_d.py
@@ -0,0 +1,40 @@
+# Part D, Inherited and subclass-specific behaviour
+
+# 1. Create a base class Employee with name and a method get_information()
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+    def get_information(self):
+        return f"{self.name} is a {type(self).__name__}"
+
+
+# 2. Create Developer(Employee) and add a method that only Developer has
+class Developer(Employee):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def start_coding(self):
+        return f"{self.name} is now coding!"
+
+
+# 3. Create another Employee subclass and give it its own sub-class specific method
+class CoffeeBringer(Employee):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def get_coffee(self):
+        return f"{self.name} is now bringing coffee!"
+
+
+# 4. Demonstrate that both subclasses can use inherited behaviour from Employee
+developer_1 = Developer("Maya")
+coffee_bringer_1 = CoffeeBringer("Jordan")
+
+print(developer_1.get_information())
+print(coffee_bringer_1.get_information())
+
+
+# 5. Demonstrate that an Employee object can't automatically use a method that only exists in one of its subclasses
+employee_1 = Employee("Eric")
+#print(employee_1.start_coding())  # AttributeError, Employee has no attribute 'start_coding'
